3aeab01c7a843359158258f5baa2789fe971fa88
ErickLozano12 <erlozanoma@ucq.edu.mx>
2025-02-11 22:06:34 -0600

    SelectorBox

--- KiwiAgents-Simulation/Assets/convencimiento/UnitSelectionManager.cs
+++ KiwiAgents-Simulation/Assets/convencimiento/UnitSelectionManager.cs
@@ -72,8 +72,6 @@ public class UnitSelectionManager : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-
             //SI es hit del click es un objeto seleccionable
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
             {
@@ -88,44 +86,49 @@ public class UnitSelectionManager : MonoBehaviour
        if (unitsSelected.Contains(unit) ==false)
         {
             unitsSelected.Add(unit);
-            TriggerSelectionIndicator(unit, true);
-
-            EnableUnitMovement(unit, true);
+            SelectUnit(unit, true);
         }
         else
         {
-            EnableUnitMovement(unit, false);
-            TriggerSelectionIndicator(unit, false);
+            SelectUnit(unit, false);
             unitsSelected.Remove(unit);
         }
     }
-    private void DeselectAll()
+  public void DeselectAll()
     {
-
         foreach (var unit in unitsSelected)
         {
-            EnableUnitMovement(unit, false);
-            TriggerSelectionIndicator(unit, false);
+            SelectUnit(unit,false);
         }
 
         groundMarker.SetActive(false);
 
         unitsSelected.Clear();
 
-
     }
+    internal void DragSelect(GameObject unit)
+    {
+       if(unitsSelected.Contains(unit) == false)
+        {
+            unitsSelected.Add(unit);
+            SelectUnit(unit, true);
+        }
+    }
+
     private void   SelectByClicking(GameObject unit)
     {
         DeselectAll();
-
-
-
         unitsSelected.Add(unit);
 
-        TriggerSelectionIndicator(unit, true);
-        EnableUnitMovement(unit, true);
+        SelectUnit(unit,true);
+    }
 
 
+
+    private void SelectUnit(GameObject unit, bool isSelected)
+    {
+        TriggerSelectionIndicator(unit,isSelected);
+        EnableUnitMovement(unit, isSelected);
     }
 
     private void EnableUnitMovement(GameObject unit, bool shouldMove)
@@ -138,4 +141,6 @@ public class UnitSelectionManager : MonoBehaviour
     {
         unit.transform.GetChild(0).gameObject.SetActive(isVisible);
     }
+
+ 
 }
