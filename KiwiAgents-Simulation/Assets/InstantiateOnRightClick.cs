d7b6145f9b433e379dcb9b7d7b8a67e04ff2c32c
Ángel Carbajal <aresendiz29198@ucq.edu.mx>
2025-02-16 23:55:23 -0600

    Popó mejorada
    La popó ya no se suelta con el clic derecho, ahora se suelta con la tecla "C". Además se incorporó una función para que suelte la popó de manera automática que oscila entre los 10 segundos y los 30 segundos.

--- KiwiAgents-Simulation/Assets/InstantiateOnRightClick.cs
+++ KiwiAgents-Simulation/Assets/InstantiateOnRightClick.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class InstantiateOnRightClick : MonoBehaviour
 {
     [SerializeField] private GameObject objectToInstantiate; // Objeto a instanciar
     [SerializeField] private Transform spawnPoint; // Punto de spawn del objeto
+    
+    private void Start()
+    {
+        StartCoroutine(DropPoopRandomly()); // Iniciar la rutina de drop aleatorio
+    }
 
     private void Update()
     {
-        if (Mouse.current.rightButton.wasPressedThisFrame) // Detecta clic derecho
+        if (Keyboard.current.cKey.wasPressedThisFrame) // Detecta la tecla C
         {
+            InstantiateObject();
+        }
+    }
+
+    private IEnumerator DropPoopRandomly()
+    {
+        while (true)
+        {
+            float waitTime = Random.Range(10f, 30f); // Tiempo aleatorio entre 10 y 30 segundos
+            yield return new WaitForSeconds(waitTime);
             InstantiateObject();
         }
     }
@@ -19,6 +35,7 @@ public class InstantiateOnRightClick : MonoBehaviour
         if (objectToInstantiate != null && spawnPoint != null)
         {
             Instantiate(objectToInstantiate, spawnPoint.position, spawnPoint.rotation);
+            Debug.Log("Popó soltada en: " + spawnPoint.position);
         }
         else
         {
