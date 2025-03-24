2f8c070b0068c83e7ed94c2bb73ea12a4a84b0f5
69-bits <jhon.alejandro0245@gmail.com>
2025-03-24 07:50:49 -0600

    Revert "Inclusion de los ultimos detalles de VHS shader"
    This reverts commit 554b61bbe65f2b20f2a8b4be90bf2eed2c59aeec.

--- KiwiAgents-Simulation/Assets/ImputAccions/CustomAccion.cs
+++ KiwiAgents-Simulation/Assets/ImputAccions/CustomAccion.cs
@@ -57,7 +57,7 @@ public partial class @CustomAccion: IInputActionCollection2, IDisposable
                 {
                     ""name"": ""Scanner"",
                     ""type"": ""Button"",
-                    ""id"": ""fabc9ca8-6d75-4277-8851-c43897880ac2"",
+                    ""id"": ""e14b31a4-235f-4b81-bfbd-0005ebcc1fca"",
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
@@ -100,7 +100,7 @@ public partial class @CustomAccion: IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""17295bc5-e2f8-40f8-a8b7-a40d904fa55e"",
+                    ""id"": ""95d80aeb-c8da-458b-8927-5b5fa3426639"",
                     ""path"": ""<Keyboard>/f"",
                     ""interactions"": """",
                     ""processors"": """",
